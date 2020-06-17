739a52174066248b746b6faf525ec7168299012c
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-06-17 23:09:48 +0200

    Drop Region table

--- models/Device.js
+++ models/Device.js
@@ -4,7 +4,6 @@ const DeviceTable = db.defineTable('Device', {
     Name: db.ColTypes.varchar(100).notNull(),
     Module: db.ColTypes.varchar(100),
     DisplayName: db.ColTypes.varchar(100),
-    Region: db.ColTypes.int(11),
   },
 });
 
