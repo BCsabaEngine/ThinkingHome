79ab426bede0936f2ea6322be1285cf95c30f244
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-06-24 21:28:13 +0200

    RuleCodeLog

---
+++ models/RuleCodeLog.js
@@ -0,0 +1,22 @@
+const RuleCodeLogTable = db.defineTable('RuleCodeLog', {
+  columns: {
+    Id: db.ColTypes.int(11).notNull().primaryKey().autoIncrement(),
+    DateTime: db.ColTypes.datetime().notNull().defaultCurrentTimestamp(),
+    Message: db.ColTypes.varchar(512),
+  },
+});
+
+const RuleCodeLog = {
+
+  async Insert(message) {
+    await RuleCodeLogTable.insert({ Message: message });
+  },
+
+  async GetLastLogs(limit = 100) {
+    const rows = await RuleCodeLogTable.select('*', 'ORDER BY Id DESC, LIMIT ?', [limit]);
+    return rows;
+  },
+
+};
+
+module.exports = RuleCodeLog;
