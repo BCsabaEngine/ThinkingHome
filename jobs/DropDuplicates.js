4dc3655b876ea9a9fce5fe92566acc5965b42906
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-07-25 11:50:49 +0200

    Night job logs

--- jobs/DropDuplicates.js
+++ jobs/DropDuplicates.js
@@ -3,10 +3,10 @@ const schedule = require('node-schedule');
 // Every day at 2:00
 const j = schedule.scheduleJob('0 2 * * *', function () {
 
-  logger.debug("[Jobs] DropDuplicates.Stat");
-  db.query("DELETE FROM DeviceStat WHERE EXISTS (SELECT 1 FROM DeviceStat ds2 WHERE ds2.DateTime = DeviceStat.DateTime AND ds2.Device = DeviceStat.Device AND ds2.Stat = DeviceStat.Stat AND ds2.Data = DeviceStat.Data AND ds2.Id > DeviceStat.Id)");
+  db.pquery("DELETE FROM DeviceStat WHERE EXISTS (SELECT 1 FROM DeviceStat ds2 WHERE ds2.DateTime = DeviceStat.DateTime AND ds2.Device = DeviceStat.Device AND ds2.Stat = DeviceStat.Stat AND ds2.Data = DeviceStat.Data AND ds2.Id > DeviceStat.Id)")
+    .then(x => logger.info(`[Jobs] DropDuplicates.Stat: ${x.affectedRows} rows`));
 
-  logger.debug("[Jobs] DropDuplicates.Tele");
-  db.query("DELETE FROM DeviceTele WHERE EXISTS (SELECT 1 FROM DeviceTele dt2 WHERE dt2.DateTime = DeviceTele.DateTime AND dt2.Device = DeviceTele.Device AND dt2.Telemetry = DeviceTele.Telemetry AND dt2.Data = DeviceTele.Data AND dt2.Id > DeviceTele.Id)");
-  
+  db.pquery("DELETE FROM DeviceTele WHERE EXISTS (SELECT 1 FROM DeviceTele dt2 WHERE dt2.DateTime = DeviceTele.DateTime AND dt2.Device = DeviceTele.Device AND dt2.Telemetry = DeviceTele.Telemetry AND dt2.Data = DeviceTele.Data AND dt2.Id > DeviceTele.Id)")
+    .then(x => logger.info(`[Jobs] DropDuplicates.Tele: ${x.affectedRows} rows`));
+
 });
--- jobs/DropOld.js
+++ jobs/DropOld.js
@@ -3,19 +3,19 @@ const schedule = require('node-schedule');
 // Every day at 4:00
 const j = schedule.scheduleJob('0 4 * * *', function () {
 
-  logger.debug("[Jobs] DropOld/DeviceLog");
-  db.query("DELETE FROM DeviceLog WHERE DateTime < NOW() - INTERVAL 90 DAY");
+  db.pquery("DELETE FROM DeviceLog WHERE DateTime < NOW() - INTERVAL 90 DAY")
+    .then(x => logger.info(`[Jobs] DropOld/DeviceLog: ${x.affectedRows} rows`));
 
-  logger.debug("[Jobs] DropOld/DeviceSys");
-  db.query("DELETE FROM DeviceSys WHERE DateTime < NOW() - INTERVAL 90 DAY");
+  db.pquery("DELETE FROM DeviceSys WHERE DateTime < NOW() - INTERVAL 90 DAY")
+    .then(x => logger.info(`[Jobs] DropOld/DeviceSys: ${x.affectedRows} rows`));
 
-  logger.debug("[Jobs] DropOld/Mqtt");
-  db.query("DELETE FROM Mqtt WHERE DateTime < NOW() - INTERVAL 90 DAY");
+  db.pquery("DELETE FROM Mqtt WHERE DateTime < NOW() - INTERVAL 90 DAY")
+    .then(x => logger.info(`[Jobs] DropOld/Mqtt: ${x.affectedRows} rows`));
 
-  logger.debug("[Jobs] DropOld/RuleCode");
-  db.query("DELETE FROM RuleCode WHERE EXISTS(SELECT 1 FROM RuleCode rc2 WHERE rc2.Id > RuleCode.Id) AND RuleCode.DateTime < NOW() - INTERVAL 90 DAY");
+  db.pquery("DELETE FROM RuleCode WHERE EXISTS(SELECT 1 FROM RuleCode rc2 WHERE rc2.Id > RuleCode.Id) AND RuleCode.DateTime < NOW() - INTERVAL 90 DAY")
+    .then(x => logger.info(`[Jobs] DropOld/RuleCode: ${x.affectedRows} rows`));
 
-  logger.debug("[Jobs] DropOld/RuleCodeLog");
-  db.query("DELETE FROM RuleCodeLog WHERE DateTime < NOW() - INTERVAL 90 DAY");
+  db.pquery("DELETE FROM RuleCodeLog WHERE DateTime < NOW() - INTERVAL 90 DAY")
+    .then(x => logger.info(`[Jobs] DropOld/RuleCodeLog: ${x.affectedRows} rows`));
 
 });
