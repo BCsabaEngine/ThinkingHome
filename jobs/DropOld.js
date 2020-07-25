09db1978f464d8b12cd53e3d4a04019e6ac80893
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-07-25 12:38:59 +0200

    Update DropOld.js

--- jobs/DropOld.js
+++ jobs/DropOld.js
@@ -6,10 +6,10 @@ const j = schedule.scheduleJob('0 4 * * *', function () {
   db.pquery("DELETE FROM DeviceLog WHERE DateTime < NOW() - INTERVAL 90 DAY")
     .then(x => logger.info(`[Jobs] DropOld/DeviceLog: ${x.affectedRows} rows`));
 
-  db.pquery("DELETE FROM DeviceSys WHERE DateTime < NOW() - INTERVAL 90 DAY")
+  db.pquery("DELETE FROM DeviceSys WHERE DateTime < NOW() - INTERVAL 30 DAY")
     .then(x => logger.info(`[Jobs] DropOld/DeviceSys: ${x.affectedRows} rows`));
 
-  db.pquery("DELETE FROM Mqtt WHERE DateTime < NOW() - INTERVAL 90 DAY")
+  db.pquery("DELETE FROM Mqtt WHERE DateTime < NOW() - INTERVAL 30 DAY")
     .then(x => logger.info(`[Jobs] DropOld/Mqtt: ${x.affectedRows} rows`));
 
   db.pquery("DELETE FROM RuleCode WHERE EXISTS(SELECT 1 FROM RuleCode rc2 WHERE rc2.Id > RuleCode.Id) AND RuleCode.DateTime < NOW() - INTERVAL 90 DAY")
