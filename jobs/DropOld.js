9e04bf827fe5f689d94f5778c3ff83b037d6492d
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-08-13 23:26:50 +0200

    Update DropOld.js

--- jobs/DropOld.js
+++ jobs/DropOld.js
@@ -12,6 +12,9 @@ const j = schedule.scheduleJob('0 4 * * *', function () {
   db.pquery("DELETE FROM Mqtt WHERE DateTime < NOW() - INTERVAL 30 DAY")
     .then(x => logger.info(`[Jobs] DropOld/Mqtt: ${x.affectedRows} rows`));
 
+  db.pquery("DELETE FROM Mqtt WHERE UnknownDevice IS NOT NULL AND DateTime < NOW() - INTERVAL 2 DAY")
+    .then(x => logger.info(`[Jobs] DropOld/Mqtt unknown device: ${x.affectedRows} rows`));
+
   db.pquery("DELETE FROM RuleCode WHERE EXISTS(SELECT 1 FROM RuleCode rc2 WHERE rc2.Id > RuleCode.Id) AND RuleCode.DateTime < NOW() - INTERVAL 90 DAY")
     .then(x => logger.info(`[Jobs] DropOld/RuleCode: ${x.affectedRows} rows`));
 
