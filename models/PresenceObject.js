0dbcb0319ccdc53d4e0688e30f64ad6e49233e9c
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-08-16 18:35:03 +0200

    Presence by BLE

--- models/PresenceObject.js
+++ models/PresenceObject.js
@@ -3,17 +3,12 @@ const PresenceObjectTable = db.defineTable('PresenceObject', {
     Id: db.ColTypes.int(11).notNull().primaryKey().autoIncrement(),
     Name: db.ColTypes.varchar(100).notNull(),
     DisplayName: db.ColTypes.varchar(100),
-    NetworkMac: db.ColTypes.varchar(24).notNull(),
     FaIcon: db.ColTypes.varchar(20),
     Priority: db.ColTypes.int(11).notNull(),
   },
 });
 
 const PresenceObject = {
-
-  Insert(name, networkmac) {
-    return PresenceObjectTable.insert({ Name: name, NetworkMac: networkmac });
-  },
 
   GetAllPriorityOrder() {
     return PresenceObjectTable.select('*', 'ORDER BY Priority, Name');
