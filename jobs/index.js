f5e154bb6fc81759acf022c72615aaae0222cd3b
SymbolCsaba <balazs.csaba@symboltech.hu>
2020-10-01 16:47:07 +0200

    Drop All 1

--- jobs/index.js
+++
@@ -1,9 +0,0 @@
-const glob = require('glob');
-const path = require('path');
-
-module.exports = () => {
-  glob.sync('./jobs/*.js').forEach(function (file) {
-    if (!file.endsWith("index.js"))
-      require(path.resolve(file));
-  });
-}
--- models/index.js
+++
@@ -1,9 +0,0 @@
-const glob = require('glob');
-const path = require('path');
-
-module.exports = () => {
-  glob.sync('./models/*.js').forEach(function (file) {
-    if (!file.endsWith("index.js"))
-      require(path.resolve(file));
-  });
-}
