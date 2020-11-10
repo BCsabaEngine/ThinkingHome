629fbdebafc22ed539edafb51e9d96d5f6859a5e
BCsabaEngine <csaba@balazs-magyar.hu>
2020-11-10 10:55:28 +0100

    boxed variables

--- models/PlatformSetting.js
+++ models/PlatformSetting.js
@@ -1,3 +1,5 @@
+const stringUtils = require('../lib/stringUtils')
+
 const PlatformSettingTable = db.defineTable('PlatformSetting', {
   columns: {
     Platform: db.ColTypes.varchar(100).notNull(),
@@ -14,18 +16,20 @@ const PlatformSettingModel = {
 
   async GetSettingsSync(platformcode) {
     const result = {}
-    for (const row of await PlatformSettingTable.select(['Name', 'Value'], 'WHERE Platform = ?', [platformcode])) { result[row.Name] = row.Value }
+    for (const row of await PlatformSettingTable.select(['Name', 'Value'], 'WHERE Platform = ?', [platformcode])) {
+      result[row.Name] = stringUtils.unbox(row.Value)
+    }
     return result
   },
 
   async UpdateSettingSync(platformcode, name, value) {
     await PlatformSettingTable.delete('WHERE Platform = ? AND Name = ?', [platformcode, name])
-    if (value !== undefined) { await PlatformSettingTable.insert({ Platform: platformcode, Name: name, Value: value }) }
+    if (value !== undefined) { await PlatformSettingTable.insert({ Platform: platformcode, Name: name, Value: stringUtils.box(value) }) }
   },
 
   async UpdateSettingsSync(platformcode, settingobj) {
     await PlatformSettingTable.delete('WHERE Platform = ?', [platformcode])
-    for (const [name, value] of Object.entries(settingobj)) { await PlatformSettingTable.insert({ Platform: platformcode, Name: name, Value: value }) }
+    for (const [name, value] of Object.entries(settingobj)) { await PlatformSettingTable.insert({ Platform: platformcode, Name: name, Value: stringUtils.box(value) }) }
   }
 
 }
