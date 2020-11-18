d166e2a8cf6b56a8dc191f832f27dfce19e83ff0
BCsabaEngine <csaba@balazs-magyar.hu>
2020-11-18 01:01:32 +0100

    LG TV select sources

--- platforms/Media/Devices/LgTv.js
+++ platforms/Media/Devices/LgTv.js
@@ -1,13 +1,18 @@
 const lgtv2 = require('lgtv2')
 const wakeonlan = require('wakeonlan')
 
+const ExpirationQueue = require('../../../lib/ExpirationQueue')
 const MediaDevice = require('../MediaDevice')
 const { Entity, TelemetryEntity, BoolStateEntity } = require('../../Entity')
 const { NumericValueGaugeBoardItem } = require('../../BoardItem')
 const { ButtonAction } = require('../../Action')
 
+const reconnectsecs = 5
+const queuesecs = 20
+
 class LgTv extends MediaDevice {
   lgtvcli = null;
+  cmdqueue = new ExpirationQueue()
 
   setting = {
     ip: '',
@@ -50,7 +55,6 @@ class LgTv extends MediaDevice {
 
     state: new BoolStateEntity(this, 'state', 'State', 'fa fa-toggle-on')
       .AddBoardItem(new NumericValueGaugeBoardItem())
-      .InitLastState()
       .AddAction(new ButtonAction(this, 'switchoff', 'Switch Off', 'fa fa-toggle-off', () => {
         if (!this.lgtvcli) return
         this.lgtvcli.request('ssap://system/turnOff')
@@ -65,34 +69,87 @@ class LgTv extends MediaDevice {
       .InitLastValue()
       .AddAction(new ButtonAction(this, 'mute', 'Mute', 'fa fa-volume-mute', () => {
         if (!this.lgtvcli) return
-        this.lgtvcli.request('ssap://audio/setMute', { mute: this.volume.value })
+
+        const fnc = function () {
+          this.lgtvcli.request('ssap://audio/setMute', { mute: this.volume.value })
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
       }))
       .AddAction(new ButtonAction(this, 'volumeup', 'Volume Up', 'fa fa-volume-up', () => {
         if (!this.lgtvcli) return
-        this.lgtvcli.request('ssap://audio/setMute', { mute: false }, function (err, res) {
-          if (err) return
-          this.lgtvcli.request('ssap://audio/setVolume', { volume: this.volume.value + 1 })
-        }.bind(this))
+
+        const fnc = function () {
+          this.lgtvcli.request('ssap://audio/setMute', { mute: false }, function (err, res) {
+            if (err) return
+            this.lgtvcli.request('ssap://audio/setVolume', { volume: this.volume.value + 1 })
+          }.bind(this))
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
       }))
       .AddAction(new ButtonAction(this, 'volumedown', 'Volume Down', 'fa fa-volume-down', () => {
         if (!this.lgtvcli) return
-        this.lgtvcli.request('ssap://audio/setMute', { mute: false }, function (err, res) {
-          if (err) return
-          this.lgtvcli.request('ssap://audio/setVolume', { volume: this.volume.value - 1 })
-        }.bind(this))
+
+        const fnc = function () {
+          this.lgtvcli.request('ssap://audio/setMute', { mute: false }, function (err, res) {
+            if (err) return
+            this.lgtvcli.request('ssap://audio/setVolume', { volume: this.volume.value - 1 })
+          }.bind(this))
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
+      }))
+      .AddAction(new ButtonAction(this, 'volume', 'Volume', 'fa fa-volume-up', (volume) => {
+        if (!this.lgtvcli) return
+
+        const fnc = function () {
+          this.lgtvcli.request('ssap://audio/setMute', { mute: false }, function (err, res) {
+            if (err) return
+            this.lgtvcli.request('ssap://audio/setVolume', { volume: volume })
+          }.bind(this))
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
       }))
       .AddBoardItem(new NumericValueGaugeBoardItem()),
+
+    app: new Entity(this, 'app', 'Application', 'fa fa-dice-d20')
+      .AddAction(new ButtonAction(this, 'launch', 'Launch', 'fa fa-dice-d20', (id) => {
+        if (!this.lgtvcli) return
+
+        const fnc = function () {
+          this.lgtvcli.request('ssap://system.launcher/launch', { id: id })
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
+      })),
 
     toast: new Entity(this, 'toast', 'Toast', 'fa fa-comment-dots')
       .AddAction(new ButtonAction(this, 'test', 'Test message', 'fa fa-comment-dots', () => {
         if (!this.lgtvcli) return
 
-        this.lgtvcli.request('ssap://system.notifications/createToast', { message: 'Hello! This is a test message.' })
+        const fnc = function () {
+          this.lgtvcli.request('ssap://system.notifications/createToast', { message: 'Hello! This is a test message.' })
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
       }))
       .AddAction(new ButtonAction(this, 'show', 'Show message', 'fa fa-comment-dots', (message) => {
         if (!this.lgtvcli) return
 
-        this.lgtvcli.request('ssap://system.notifications/createToast', { message: message })
+        const fnc = function () {
+          this.lgtvcli.request('ssap://system.notifications/createToast', { message: message })
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
       }))
 
   };
@@ -103,7 +160,7 @@ class LgTv extends MediaDevice {
   lgCountry = '';
   lgLanguageCode = '';
 
-  lgInputs = [];
+  // lgInputs = [];
   lgLaunchPoints = [];
 
   GetStatusInfos() {
@@ -118,13 +175,46 @@ class LgTv extends MediaDevice {
     return result
   }
 
+  InitApps() {
+    for (const app of [
+      { code: 'livetv', name: 'Live TV', id: 'com.webos.app.livetv' },
+      { code: 'av1', name: 'AV', id: 'com.webos.app.externalinput.av1' },
+      { code: 'hdmi1', name: 'HDMI-1', id: 'com.webos.app.hdmi1' },
+      { code: 'hdmi2', name: 'HDMI-2', id: 'com.webos.app.hdmi2' },
+      { code: 'hdmi3', name: 'HDMI-3', id: 'com.webos.app.hdmi3' },
+      { code: 'hdmi4', name: 'HDMI-4', id: 'com.webos.app.hdmi4' },
+
+      { code: 'music', name: 'Music', id: 'com.webos.app.music' },
+      { code: 'video', name: 'Video', id: 'com.webos.app.photovideo' },
+      { code: 'youtube', name: 'Youtube', id: 'youtube.leanback.v4' },
+      { code: 'netflix', name: 'Netflix', id: 'netflix' },
+      { code: 'spotify', name: 'Spotify', id: 'spotify-beehive' },
+
+      { code: 'browser', name: 'Browser', id: 'com.webos.app.browser' },
+      { code: 'miracast', name: 'Screen share', id: 'com.webos.app.miracast' }
+    ]) {
+      this.entities.app.AddAction(new ButtonAction(this, app.code, app.name, 'fa fa-dice-d20', () => {
+        if (!this.lgtvcli) return
+
+        const fnc = function () {
+          this.lgtvcli.request('ssap://system.launcher/launch', { id: app.id })
+        }.bind(this)
+
+        if (this.entities.state.state) fnc()
+        else this.cmdqueue.add(fnc, queuesecs * 1000)
+      }))
+    }
+  }
+
   async Start() {
     await super.Start()
+    this.InitApps()
     this.InitCli()
   }
 
   OnConnect() {
     this.entities.state.SetState(true)
+
     this.lgtvcli.request('ssap://com.webos.service.update/getCurrentSWInformation', function (err, res) {
       if (!err && res.returnValue) {
         this.lgProductName = res.product_name
@@ -133,6 +223,23 @@ class LgTv extends MediaDevice {
         this.lgCountry = res.country_group + '-' + res.country
         this.lgLanguageCode = res.language_code
       }
+    }.bind(this))
+
+    this.lgtvcli.request('ssap://com.webos.applicationManager/listLaunchPoints', function (err, res) {
+      if (err) return
+      if (!res.returnValue) return
+
+      this.lgLaunchPoints = res.launchPoints
+      // console.log(res.launchPoints)
+    }.bind(this))
+
+    this.lgtvcli.request('ssap://com.webos.applicationManager/getForegroundAppInfo', function (err, res) {
+      if (err) return
+      if (!res.returnValue) return
+
+      const id = res.appId
+      const app = this.lgLaunchPoints.find((i) => i.id === id)
+      if (app) this.entities.app.name = app.title
     }.bind(this))
 
     this.lgtvcli.subscribe('ssap://audio/getVolume', function (err, res) {
@@ -146,25 +253,15 @@ class LgTv extends MediaDevice {
       if (err) return
       if (!res.returnValue) return
 
-      // this res.appId
-      // console.log(res)
+      const id = res.appId
+      const app = this.lgLaunchPoints.find((i) => i.id === id)
+      if (app) this.entities.app.name = app.title
     }.bind(this))
 
-    this.lgtvcli.request('ssap://tv/getExternalInputList', function (err, res) {
-      if (err) return
-      if (!res.returnValue) return
-
-      this.lgInputs = res
-      // console.log(res)
-    }.bind(this))
-
-    this.lgtvcli.request('ssap://com.webos.applicationManager/listLaunchPoints', function (err, res) {
-      if (err) return
-      if (!res.returnValue) return
-
-      this.lgLaunchPoints = res
-      // console.log(res)
-    }.bind(this))
+    while (!this.cmdqueue.empty) {
+      const qitem = this.cmdqueue.get()
+      if (typeof qitem === 'function') qitem()
+    }
   }
 
   InitCli() {
@@ -176,7 +273,7 @@ class LgTv extends MediaDevice {
       this.lgtvcli = lgtv2({
         url: `ws://${this.setting.ip}:3000`,
         timeout: 500,
-        reconnect: 10 * 1000,
+        reconnect: reconnectsecs * 1000,
         saveKey: function (key) { if (this.setting.tvkey !== key) this.AdaptSetting('tvkey', key) }.bind(this),
         clientKey: this.setting.tvkey
       })
