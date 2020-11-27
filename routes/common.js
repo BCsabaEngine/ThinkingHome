e081a0712ad889f3fca416db373647c79db8fdeb
BCsabaEngine <csaba@balazs-magyar.hu>
2020-11-27 16:28:08 +0100

    Common variables replace

--- routes/common.js
+++ routes/common.js
@@ -1,6 +1,8 @@
 const ipUtils = require('../lib/ipUtils')
 const WebAccessModel = require('../models/WebAccess')
+const dayjs = require('dayjs')
 const UserNotifyModel = require('../models/UserNotify')
+const stringUtils = require('../lib/stringUtils')
 
 module.exports = (app) => {
   // log all request
@@ -15,8 +17,14 @@ module.exports = (app) => {
     return next()
   })
 
+  // variables available in all request
   app.use(async function (req, res, next) {
+    app.locals.truncate = stringUtils.truncate
+    app.locals.middlemask = stringUtils.middlemask
+    app.locals.dayjs = dayjs
+
     const user = app.getUser(req)
+    app.locals.user = user || {}
     app.locals.usernotifies = user && user.id ? await await UserNotifyModel.GetUnreadByUserIdSync(user.id) : []
 
     return next()
