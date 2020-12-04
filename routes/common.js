dc4f4e70738fc01512d972f27a6202c707aca267
BCsabaEngine <csaba@balazs-magyar.hu>
2020-12-04 18:40:25 +0100

    refactor

--- routes/common.js
+++ routes/common.js
@@ -1,22 +1,11 @@
+const dayjs = require('dayjs')
+
 const ipUtils = require('../lib/ipUtils')
 const WebAccessModel = require('../models/WebAccess')
-const dayjs = require('dayjs')
 const UserNotifyModel = require('../models/UserNotify')
 const stringUtils = require('../lib/stringUtils')
 
 module.exports = (app) => {
-  // log all request
-  app.use(function (req, res, next) {
-    const user = req.session && req.session.user ? req.session.user.id : null
-    const uri = req.originalUrl
-    const session = req.sessionID
-    const requestip = ipUtils.remoteip(req)
-
-    WebAccessModel.Insert(user, uri, session, requestip)
-
-    return next()
-  })
-
   // variables available in all request
   app.use(async function (req, res, next) {
     app.locals.truncate = stringUtils.truncate
@@ -26,6 +15,18 @@ module.exports = (app) => {
     const user = app.getUser(req)
     app.locals.user = user || {}
     app.locals.usernotifies = user && user.id ? await await UserNotifyModel.GetUnreadByUserIdSync(user.id) : []
+
+    return next()
+  })
+
+  // log all request
+  app.use(function (req, res, next) {
+    const user = req.session && req.session.user ? req.session.user.id : null
+    const uri = req.originalUrl
+    const session = req.sessionID
+    const requestip = ipUtils.remoteip(req)
+
+    WebAccessModel.Insert(user, uri, session, requestip)
 
     return next()
   })
