8d98da495ef9d895e318cdf0513f4d34d5b9cf26
BCsabaEngine <csaba@balazs-magyar.hu>
2020-12-10 19:38:01 +0100

    Update ThSpeak.js

--- platforms/Mqtt/Devices/ThSpeak.js
+++ platforms/Mqtt/Devices/ThSpeak.js
@@ -1,3 +1,8 @@
+const os = require('os')
+const { isInSubnet } = require('subnet-check')
+const got = require('got')
+const FormData = require('form-data')
+
 const { Entity } = require('../../Entity')
 const { ButtonAction } = require('../../Action')
 const Thinking = require('./Thinking')
@@ -6,10 +11,11 @@ class ThSpeak extends Thinking {
   get icon() { return 'fa fa-bullhorn' }
   entities = {
     speak: new Entity(this, 'speak', 'Speak', 'fa fa-bullhorn')
-      .AddAction(new ButtonAction(this, 'test', 'Test speak', 'fa fa-bullhorn', () => {
-        this.GetOrGenerateMp3('x')
-        this.SendCmd('playmp3', { url: 'http://brain.thinkinghome.hu/hello.mp3', volume: 10 })
-      }))
+      .AddAction(new ButtonAction(this, 'test', 'Test speak', 'fa fa-bullhorn', async function () {
+        const ids = await this.GetMp3Ids(['Szia', 'Hello', 'Sziasztok'], ['Én vagyok Csaba', 'Én apa vagyok'])
+        console.log(ids)
+        // this.SendCmd('playmp3', { url: 'http://brain.thinkinghome.hu/hello.mp3', volume: 10 })
+      }.bind(this)))
       .AddAction(new ButtonAction(this, 'play', 'Play sentence', 'fa fa-bullhorn', (...args) => {
         // console.log(args)
       }))
@@ -19,11 +25,67 @@ class ThSpeak extends Thinking {
     toDisplayList: function () { return [] },
     toTitle: function () { return this.constructor.name }.bind(this),
     toSubTitle: function () { return '' }
-  };
+  }
 
-  GetOrGenerateMp3(text) {
-    const keys = this.GetDataKeys()
-    if (keys && keys.length && keys.includes(text)) { }
+  GetStatusInfos() {
+    const result = super.GetStatusInfos()
+    const ip = this.GetThinkingHomeIp()
+    if (ip) { result.push({ device: this, message: __('TH IP address'), value: ip }) }
+    return result
+  }
+
+  GetThinkingHomeIp() {
+    if (!this.thinkingIPAddress) return ''
+
+    const nets = os.networkInterfaces()
+
+    for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+        if (net.family === 'IPv4' && !net.internal && net.cidr) {
+          if (isInSubnet(this.thinkingIPAddress, net.cidr)) {
+            return net.address
+          }
+        }
+      }
+    }
+  }
+
+  async GetMp3Id(text) {
+    let keys = await this.GetDataKeys()
+    for (const id of Object.keys(keys)) {
+      if (keys[id] === text) return id
+    }
+
+    if (!systemsettings.CloudToken) throw new Error('Token not set')
+
+    const form = new FormData()
+    form.append('token', systemsettings.CloudToken)
+    form.append('text', text)
+
+    const mp3response = await got.post(config.brainserver.server + config.brainserver.ttsservice, { body: form })
+    await this.UpdateDataByKey(text, mp3response.body)
+
+    keys = await this.GetDataKeys()
+    for (const id of Object.keys(keys)) {
+      if (keys[id] === text) return id
+    }
+    throw new Error('MP3 not found')
+  }
+
+  async GetMp3Ids(...args) {
+    const result = []
+
+    for (const arg of args) {
+      let a = arg
+      if (Array.isArray(a)) {
+        const aindex = Math.floor(Math.random() * a.length)
+        a = a[aindex]
+      }
+
+      result.push(await this.GetMp3Id(a))
+    }
+
+    return result
   }
 
   // ProcessMessage(topic, message) {
