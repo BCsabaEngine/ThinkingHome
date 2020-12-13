c8245493b0b38a18bf328a50d1c9b7969d2bb6e3
BCsabaEngine <csaba@balazs-magyar.hu>
2020-12-13 14:15:59 +0100

    Long press support

--- platforms/Zigbee/Devices/E1766.js
+++ platforms/Zigbee/Devices/E1766.js
@@ -2,16 +2,31 @@ const GenericDevice = require('./GenericDevice')
 const { EventEntity } = require('../../Entity')
 
 class E1766 extends GenericDevice {
+  lastdirection = ''
+
   ProcessSpecificMessageObj(messageobj) {
     switch (messageobj.action) {
       case 'open':
         this.entities.pushbutton.DoEvent('open')
+        this.lastdirection = 'open'
         break
       case 'close':
         this.entities.pushbutton.DoEvent('close')
+        this.lastdirection = 'close'
         break
       case 'stop':
         this.entities.pushbutton.DoEvent('stop')
+        switch (this.lastdirection) {
+          case 'open':
+            this.entities.pushbutton.DoEvent('openstop')
+            break
+          case 'close':
+            this.entities.pushbutton.DoEvent('closestop')
+            break
+          default:
+            break
+        }
+        this.lastdirection = ''
         break
       default:
         break
@@ -20,7 +35,7 @@ class E1766 extends GenericDevice {
 
   entities = {
     pushbutton: new EventEntity(this, 'pushbutton', 'Push button', 'fa fa-toggle-on')
-      .InitEvents(['open', 'close', 'stop'])
+      .InitEvents(['open', 'close', 'stop', 'openstop', 'closestop'])
   };
 
   get icon() { return 'fa fa-toggle-on' }
