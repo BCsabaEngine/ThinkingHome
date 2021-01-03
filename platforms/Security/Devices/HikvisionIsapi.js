0260d04cbb7d0ebf0419987257965d2b500922d0
BCsabaEngine <csaba@balazs-magyar.hu>
2021-01-03 16:06:50 +0100

    Hikvision camera

--- platforms/Security/Devices/HikvisionIsapi.js
+++ platforms/Security/Devices/HikvisionIsapi.js
@@ -1,16 +1,21 @@
 const path = require('path')
 const got = require('got')
 const xmlparser = require('fast-xml-parser')
+const sharp = require('sharp')
 
 const GenericCamera = require('./GenericCamera')
 
-const uriinitpicture = '/initpicture'
+const urilastpicture = '/lastpicture'
+const urilastthumbnale = '/lastthumbnale'
+const thumbnalesize = 320
 
 class HikvisionIsapi extends GenericCamera {
   setting = {
     ip: '',
     username: '',
     password: '',
+    refreshminutes: 0,
+
     toDisplayList: function () {
       const result = {}
       result.ip = {
@@ -34,6 +39,16 @@ class HikvisionIsapi extends GenericCamera {
         error: false,
         canclear: true
       }
+      result.refreshminutes = {
+        type: 'select',
+        title: __('Refresh picture'),
+        value: this.setting.refreshminutes,
+        displayvalue: this.setting.refreshminutes ? __n('%s minute', '%s minutes', this.setting.refreshminutes) : __('Never'),
+        lookup: JSON.stringify({ 0: __('Never'), 1: 1, 2: 2, 3: 3, 5: 5, 10: 10, 15: 15, 30: 30, 60: 60 }).replace(/["]/g, "'"),
+        error: false,
+        canclear: false,
+        onchange: function () { this.InitRefreshTimer() }.bind(this)
+      }
       return result
     }.bind(this),
     toTitle: function () { return 'Hikvision ISAPI' },
@@ -48,7 +63,10 @@ class HikvisionIsapi extends GenericCamera {
   hikvisionFirmware = '';
 
   imagechannelid = null
-  initialpicture = null
+
+  lastpicture = null
+  lastpicturedimension = null
+  lastthumbnale = null
 
   GetStatusInfos() {
     const result = []
@@ -57,6 +75,19 @@ class HikvisionIsapi extends GenericCamera {
     if (this.hikvisionModel) result.push({ message: 'Model', value: `${this.hikvisionModel} (${this.hikvisionDeviceType})` })
     if (this.hikvisionFirmware) result.push({ message: __('Version'), value: this.hikvisionFirmware })
     if (this.hikvisionDeviceName) result.push({ message: __('Name'), value: this.hikvisionDeviceName })
+
+    if (this.lastpicture) {
+      result.push({ message: __('Picture size'), value: Math.round(this.lastpicture.length / 1024) + ' kB' })
+      if (this.lastpicturedimension) result.push({ message: __('Picture resolution'), value: this.lastpicturedimension + ' px' })
+      if (this.lastthumbnale) {
+        result.push({
+          // message: __('Last picture'),
+          thumbnaleurl: `/platform/security/device/${this.name}/lastthumbnale`,
+          pictureurl: `/platform/security/device/${this.name}/lastpicture`
+        })
+      }
+    } else result.push({ warning: true, message: __('Cannot get picture') })
+
     return result
   }
 
@@ -64,9 +95,28 @@ class HikvisionIsapi extends GenericCamera {
     await super.Start()
 
     await this.InitDeviceInfo()
-    await this.InitPicture()
+    await this.GetPicture()
+    this.InitRefreshTimer()
 
-    this.approuter.get(uriinitpicture, this.WebInitPicture.bind(this))
+    this.approuter.get(urilastpicture, this.WebLastPicture.bind(this))
+    this.approuter.get(urilastthumbnale, this.WebLastThumbnale.bind(this))
+  }
+
+  async GetISAPI(uri, raw = false) {
+    const url = path.join(`http://${this.setting.ip}`, uri)
+    try {
+      const options = {
+        username: this.setting.username || '',
+        password: this.setting.password || ''
+      }
+
+      const response = await got.get(url, options)
+
+      return raw ? response.rawBody : response.body
+    } catch (error) {
+      logger.error(`${error.message} with url ${url}`)
+      return null
+    }
   }
 
   async InitDeviceInfo() {
@@ -99,48 +149,64 @@ class HikvisionIsapi extends GenericCamera {
     }
   }
 
-  async InitPicture() {
-    await this.InitPictureChannel()
+  refreshtimer = null
+  InitRefreshTimer() {
+    clearInterval(this.refreshtimer)
+    this.refreshtimer = null
 
-    if (this.imagechannelid === undefined) return
-    if (this.imagechannelid < 0) return
-
-    const picture = await this.GetISAPI(`/ISAPI/Streaming/channels/${this.imagechannelid}/picture`, true)
-    this.initialpicture = picture
-
-    // this.GetISAPI('/ISAPI/System/deviceInfo')
-
-    // http://192.168.1.201/ISAPI/Image/Channels
-    // http://192.168.1.201/ISAPI/Streaming/channels/1/picture
-  }
-
-  async GetISAPI(uri, raw = false) {
-    const url = path.join(`http://${this.setting.ip}`, uri)
-    try {
-      const options = {
-        username: this.setting.username || '',
-        password: this.setting.password || ''
-      }
-
-      const response = await got.get(url, options)
-
-      return raw ? response.rawBody : response.body
-    } catch (error) {
-      logger.error(`${error.message} with url ${url}`)
-      return null
+    if (this.setting.refreshminutes) {
+      this.refreshtimer = setInterval(
+        function () { this.GetPicture() }.bind(this),
+        this.setting.refreshminutes * 60 * 1000)
     }
   }
 
-  GetPicture() {
-    return null
+  async GetPicture() {
+    await this.InitPictureChannel()
+
+    this.lastpicture = null
+
+    if (this.imagechannelid === undefined) return null
+    if (this.imagechannelid < 0) return null
+
+    const picture = await this.GetISAPI(`/ISAPI/Streaming/channels/${this.imagechannelid}/picture`, true)
+    this.lastpicture = Buffer.from(picture)
+
+    if (this.lastpicture) {
+      const image = sharp(this.lastpicture)
+
+      image
+        .metadata()
+        .then(metadata => { this.lastpicturedimension = `${metadata.width}x${metadata.height}` })
+
+      image
+        .resize(thumbnalesize)
+        .toFormat('jpeg')
+        .toBuffer()
+        .then(data => { this.lastthumbnale = data })
+        .catch(err => {
+          this.lastthumbnale = null
+          logger.warn(`[HikvisionIsapi] Cannot create thumbnail: ${err.message}`)
+        })
+    }
+
+    return picture
   }
 
-  async WebInitPicture(req, res, next) {
+  async WebLastPicture(req, res, next) {
     res.set({
       'Content-Type': 'image/jpeg',
-      'Content-Length': Buffer.byteLength(this.initialpicture)
+      'Content-Length': Buffer.byteLength(this.lastpicture)
     })
-    res.send(this.initialpicture)
+    res.send(this.lastpicture)
+  }
+
+  async WebLastThumbnale(req, res, next) {
+    res.set({
+      'Content-Type': 'image/jpeg',
+      'Content-Length': Buffer.byteLength(this.lastthumbnale)
+    })
+    res.send(this.lastthumbnale)
   }
 }
 module.exports = HikvisionIsapi
